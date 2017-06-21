7cf4d206ab4d7a3a97b0736d0292c23c68fd3c70
Mario Silva <bymario.silva@bycom.com.pt>
2017-06-21 15:12:07 +0100

    Update examples component

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ const React = require('react');
 
 let Examples = (props) => {
   return (
-    <h3>Examples Component!</h3>
+   <div>
+      <h3>Examples Component!</h3>
+      <p>Welcome to examples page!</p>
+   </div>
   )
 };
 
