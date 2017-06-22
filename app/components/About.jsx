b06701afaf6147bc5ac48e319d851a9f4b32c7aa
Mario Silva <bymario.silva@bycom.com.pt>
2017-06-22 17:30:02 +0100

    Update about page copy

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ const React = require('react');
 
 let About = (props) => {
     return (
-      <h3>About Component</h3>
+      <div>
+        <h3>About</h3>
+        <p>Welcome to About page!</p>
+      </div>
     )
 };
 
